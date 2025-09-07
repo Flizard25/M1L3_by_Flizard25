b9d9f08b61a5bfe757a1b6d9cb4d9fd674fd8eae
Flizard25 <levmajer28@gmail.com>
2025-09-07 13:00:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,4 +23,9 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'Я принял нового пользователя!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id) 
+
 bot.infinity_polling(none_stop=True)
